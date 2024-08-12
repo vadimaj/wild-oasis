34ee596be037728c7bbefb351924a8a2ef7e7cb1
Vadim Yatskul <46135148+vadimaj@users.noreply.github.com>
2024-08-12 22:25:57 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-yellow-500 bg-stone-800 text-center font-bold uppercase text-xl">
+      TailWind CSS Vite Template
+    </h1>
+  );
+}
+
+export default App;
